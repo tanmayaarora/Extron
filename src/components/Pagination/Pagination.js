adc0548b6dfeca2350b2ac98b3bd4f70a4e6b62a
tanmayaarora <96226092+tanmayaarora@users.noreply.github.com>
2021-12-16 13:36:43 +0530

    Add files via upload

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import './Pagination.css'
+import data from '../../data.json'
+
+export default function Pagination({ RenderComponent, pageLimit, dataLimit }) {
+    const [pages] = useState(Math.round(data.length / dataLimit));
+    const [currentPage, setCurrentPage] = useState(1);
+
+    function goToNextPage() {
+        setCurrentPage((page) => page + 1);
+      }
+    
+    function goToPreviousPage() {
+        setCurrentPage((page) => page - 1);
+    }
+
+    function changePage(event) {
+        const pageNumber = Number(event.target.textContent);
+        setCurrentPage(pageNumber);
+    }
+
+    const getPaginatedData = () => {
+        const startIndex = currentPage * dataLimit - dataLimit;
+        const endIndex = startIndex + dataLimit;
+        return data.slice(startIndex, endIndex);
+    }
+
+    const getPaginationGroup = () => {
+        let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+        return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+    }
+
+    return (
+        <div>
+      
+          {/* show the posts, 2 posts at a time */}
+          <div className="dataContainer">
+            {getPaginatedData().map((d, idx) => (
+              <RenderComponent key={idx} data={d} />
+            ))}
+          </div>
+      
+          {/* show the pagiantion
+              it consists of next and previous buttons
+              along with page numbers, in our case, 5 page
+              numbers at a time
+          */}
+          <div className="pagination">
+            {/* previous button */}
+            <button
+              onClick={goToPreviousPage}
+              className={`prev ${currentPage === 1 ? 'disabled' : ''}`}
+            >
+              Previous
+            </button>
+      
+            {/* show page numbers */}
+            {getPaginationGroup().map((item, index) => (
+              <button
+                key={index}
+                onClick={changePage}
+                className={`paginationItem ${currentPage === item ? 'active' : null}`}
+              >
+                <span>{item}</span>
+              </button>
+            ))}
+      
+            {/* next button */}
+            <button
+              onClick={goToNextPage}
+              className={`next ${currentPage === pages ? 'disabled' : ''}`}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+        );
+}
