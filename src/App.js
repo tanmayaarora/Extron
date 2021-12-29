636d6ec5f8d6175ce35538be27636b07cead6f96
tanmayaarora <96226092+tanmayaarora@users.noreply.github.com>
2021-12-29 17:49:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Login from './components/Login';
+import Main from './components/Main';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
