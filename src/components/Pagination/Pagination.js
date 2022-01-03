5493809465a3a1e80a0f43dfee717856ed458a4e
tanmayaarora <96226092+tanmayaarora@users.noreply.github.com>
2022-01-03 13:36:36 +0530

    Update Pagination.js

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -108,17 +108,6 @@ export default function Pagination({ RenderComponent, pageLimit }) {
               Previous
             </button>
       
-            {/* show page numbers */}
-            {/* {getPaginationGroup().map((item, index) => (
-              <button
-                key={index}
-                onClick={changePage}
-                className={`paginationItem ${currentPage === item ? 'active' : null}`}
-              >
-                <span>{item}</span>
-              </button>
-            ))} */}
-      
             {/* next button */}
             <button
               onClick={goToNextPage}
@@ -126,6 +115,7 @@ export default function Pagination({ RenderComponent, pageLimit }) {
             >
               Next
             </button>
+            
             <button
             onClick={()=>setCurrentPage(pages)}
             className={`last ${currentPage === pages ? 'disabled' : ''}`}>
