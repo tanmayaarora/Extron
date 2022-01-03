b78b0e7d6ecde98d3471bbf75815034b6e91ce7a
tanmayaarora <96226092+tanmayaarora@users.noreply.github.com>
2022-01-03 22:25:57 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,11 @@
 import Blog from './Blog/Blog'
 import Footer from './Footer/Footer';
-import Header from './Header/Header';
 import Pagination from './Pagination/Pagination';
 
 export default function Main() {
 
     return (
         <>
-        <Header/>
         <Pagination RenderComponent={Blog} pageLimit={5} />
         <Footer/>
         </>
