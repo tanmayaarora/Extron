92793938d7ecde6970ad12514f2dcb8c45bdddd3
tanmayaarora <96226092+tanmayaarora@users.noreply.github.com>
2022-01-03 13:30:28 +0530

    Add files via upload

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,16 @@
+import './Blog.scss'
+import './_grid.scss'
+
+export default function Blog(props) {
+    const { id, title, description, name, imgsource } = props.data;
+ 
+    return (
+        <div key={id} className='blog col-sm-6 col-md-3 col-lg-3'>
+            <img src={imgsource} width={320} height={300}/>
+            <h1>{title}</h1>
+            <h3><i>{description}</i></h3>
+            <h5> By {name}</h5>
+            
+        </div>
+        );
+  }
