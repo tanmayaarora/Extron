bbec7c9721404030d3222bea89a2b4aaa42ea61b
tanmayaarora <96226092+tanmayaarora@users.noreply.github.com>
2022-01-04 11:51:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Login from './components/Login/Login';
 import Main from './components/Main';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
