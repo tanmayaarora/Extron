8acdd8ca89917a268d25d95701661e0749b01dab
tanmayaarora <96226092+tanmayaarora@users.noreply.github.com>
2022-01-17 10:33:29 +0530

    Create Header.js

---
+++ src/components/frontend/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.scss'
+
+export default function Header() {
+
+    return (
+        <div className='div1'>
+            <div>
+                <a href="/" className='anchorStyle'>Home</a>
+                <h1 className='special'>T. Arora</h1>
+                <a href="#" className='anchorStyle'>About</a>
+            </div>
+        </div>
+    );
+}
