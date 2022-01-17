d9f73f2423b2fce3ca567f254b814f90124fad6d
tanmayaarora <96226092+tanmayaarora@users.noreply.github.com>
2022-01-17 10:16:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import './App.scss';
-import Login from './components/Login/Login';
-import Main from './components/Main';
+import './sass/App.scss';
+import Login from './components/frontend/User/Login';
+import Main from './components/frontend/Main';
+import Reset from './components/frontend/User/PasswordReset';
+import Register from './components/frontend/User/NewUser';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Store from './components/frontend/Store'
 
 function App() {
 
   return (
+    <Provider store={Store}>
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Main/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/reset" element={<Reset/>}/>
+          <Route path="/register" element={<Register/>}/>
         </Routes>
       </Router>
     </div>
+    </Provider>
   );
 }
 
